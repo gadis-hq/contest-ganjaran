261d89a90addf018a9b87d8c95a423e819ed87f3
Gadis QS HQ <qsgadis449@gmail.com>
2026-03-12 04:00:56 +0800

    Update map.js

--- js/map.js
+++ js/map.js
@@ -4,23 +4,49 @@ L.tileLayer(
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 ).addTo(map);
 
+fetch(CONFIG.API_URL + "?heatmap=1")
+
+.then(r=>r.json())
+
+.then(data=>{
+
+let points=[];
+
+data.forEach(p=>{
+points.push([p.lat,p.lng,p.weight]);
+});
+
+L.heatLayer(points,{
+radius:30,
+blur:25
+}).addTo(map);
+
+});
+
+
 fetch(CONFIG.API_URL + "?map=heat")
 
 .then(r=>r.json())
 
 .then(data=>{
 
-let heatPoints=[];
-
 data.forEach(p=>{
 
-if(!p.lat || !p.lng) return;
+let marker = L.marker([p.lat,p.lng]).addTo(map);
 
-heatPoints.push([
-parseFloat(p.lat),
-parseFloat(p.lng),
-0.5
-]);
+marker.bindPopup(
+`🎉 ${p.nama}<br>${p.hadiah}<br>${p.negeri}`
+);
+
+if(p.status=="TELAH DITEBUS"){
+
+marker._icon.classList.add("blink");
+
+}
+
+});
+
+});
 
 // pin lokasi pemenang
 L.marker([p.lat,p.lng])
